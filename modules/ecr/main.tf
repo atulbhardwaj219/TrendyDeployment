c6a6b1e2806e6507c23e3ad9039ca37111cd7f80
atul <atulbhardwaj219@gmail.com>
2021-05-23 15:46:33 +0530

    changed the yml file

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -2,12 +2,4 @@
 
 resource "aws_ecrpublic_repository" "public" {
   repository_name = "Build"
-
-  catalog_data {
-    about_text        = "Public Repo to build"
-    architectures     = ["ARM"]
-    description       = "Description"
-    operating_systems = ["Linux"]
-}
-
 }
